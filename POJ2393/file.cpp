1528e927422b588f42a4e057fd561d817613c446
SsorryQaQ <ssorryqaq@gmail.com>
2018-11-17 16:55:05 +0800

    POJ2393_11-17

---
+++ POJ2393/file.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <algorithm>
+#include <iostream>
+#define MAX_N 10100
+
+using namespace std;
+typedef pair<int,int> P;
+P cost_need[MAX_N];
+
+long long solve();
+int N,S;
+
+int main()
+{
+    // freopen("F:\\github\\OJ_Solution\\POJ2393\\file.in","r",stdin);
+    // freopen("F:\\github\\OJ_Solution\\POJ2393\\file.out","w",stdout);
+    scanf("%d %d",&N,&S);
+    for (int i=0;i<N;i++)
+        scanf("%d %d",&cost_need[i].first,&cost_need[i].second);
+    long long res=solve();
+    printf("%lld\n",res);
+
+    // fclose(stdin);fclose(stdout);
+    return 0;
+}
+
+long long solve()
+{
+    long long res=0;
+    for (int i=0;i<N;i++)
+    {
+        int cost=cost_need[i].first;
+        for (int j=i-1;j>=0;j--)
+        {
+            int tmp=cost_need[j].first + S*(i-j);
+            if(tmp<cost)
+                cost=tmp;
+        }
+        res+=cost*cost_need[i].second;
+    }
+    return res;
+}
--- POJ3190/file.cpp
+++ POJ3190/file.cpp
@@ -1,17 +1,57 @@
 #include <cstdio>
 #include <algorithm>
+#include <queue>
+
 #define MAX_N 51000
 
 using namespace std;
 typedef pair<int,int> P;
 
 P cow[MAX_N];
-bool used[MAX_N];
 int stalls[MAX_N];
-
+int N;
 void solve();
+
 
 int main()
 {
+    freopen("F:\\github\\OJ_Solution\\POJ3190\\file.in","r",stdin);
+    freopen("F:\\github\\OJ_Solution\\POJ3190\\file.out","w",stdout);
+    scanf("%d",&N);
+    for (int i=0;i<N;i++)
+        scanf("%d %d",&cow[i].first,&cow[i].second);
+    
+    sort(cow,cow+N);
+    // solve();
+    for (int i=0;i<N;i++)
+        printf("%d %d\n",&cow[i].first,&cow[i].second);
+    fclose(stdin);
+    fclose(stdout);
     return 0;
+}
+
+void solve()
+{
+    int res=1;
+    priority_queue<P> pq;
+    stalls[0] = 1;
+    pq.push(P(cow[0].second,1));
+    for (int i=1;i<N;i++)
+    {
+        if (cow[i].first > pq.top().first)
+        {
+            stalls[i]=pq.top().second;
+            pq.pop();
+        }
+        else
+        {
+            stalls[i]=pq.top().second+1;
+            res++;
+        }
+        pq.push(P(cow[i].second,stalls[i]));
+    }
+    printf("%d\n",res);
+    for (int i=0;i<N;i++)
+        printf("%d\n",stalls[i]);
+        
 }
